94fe823c61550f5e75b40557d4342842bfdd5336
Yvo Brevoort <yvo@muze.nl>
2023-04-05 15:25:06 +0200

    whitespace

--- www/api/data/components/sharing/transformers/wordsLink-render.js
+++ www/api/data/components/sharing/transformers/wordsLink-render.js
@@ -2,5 +2,4 @@ function(data) {
  this.originalValue = data
  let value = document.location.origin + document.location.pathname + "#words/" + data.join("/")
  return { href: value, innerHTML: value }
-  
 }

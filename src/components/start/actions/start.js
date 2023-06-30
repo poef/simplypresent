3c26394ec20970ac623830dcd517ba7e7e440e17
Auke van Slooten <auke@muze.nl>
2023-06-30 15:16:35 +0200

    removed start component, added action to the start base-component

--- src/components/start/actions/start.js
+++
@@ -1,7 +0,0 @@
-async function() {
-  this.app.started = true
-  this.app.keyboard = simply.keyboard(this.app, slidesKeyboard)
-  this.app.view.slidesTotal = 10
-  await this.call('initMenu')
-  await this.call('initSlides')
-}
